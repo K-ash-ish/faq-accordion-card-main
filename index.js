657f7734c2a37cf8cffefc21a83691e54d02abc5
K-ash-ish <kashishsondhiya@gmail.com>
2021-08-17 20:18:27 +0530

    updated files

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+
+const boldText = document.getElementsByClassName("question");
+
+for( var i = 0 ; i < boldText.length ; i++){
+    boldText[i].addEventListener("click", () => {
+        
+    })
+}
+
+
+console.log(boldText);
