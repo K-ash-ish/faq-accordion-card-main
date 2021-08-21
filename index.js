90f62070486d915b4e11426204df20509b15642a
K-ash-ish <kashishsondhiya@gmail.com>
2021-08-21 20:40:54 +0530

    new changes

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
 
 
 
-const boldText = document.getElementsByClassName("question");
-
-for( var i = 0 ; i < boldText.length ; i++){
-    boldText[i].addEventListener("click", () => {
-        console.log("CLICKED");
-    })
-}
-
-
-console.log(boldText);
